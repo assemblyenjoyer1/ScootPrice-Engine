a4c128597fd762f3c09b0e9b7d08a96f4080c357
reiss timo <t.reiss@eos-ts.com>
2023-06-27 11:57:20 +0200

    added functionality to repo

--- src/main/java/com/assemblyenjoyer1/insanecalculator/repository/IUserRepository.java
+++ src/main/java/com/assemblyenjoyer1/insanecalculator/repository/IUserRepository.java
@@ -1,7 +1,15 @@
 package com.assemblyenjoyer1.insanecalculator.repository;
 
+import com.assemblyenjoyer1.insanecalculator.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface IUserRepository extends JpaRepository {
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+@Repository
+public interface IUserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUuid(UUID uuid);
 }
