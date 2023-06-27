d445df5c626b87047dfe68979d45921e8b7b08df
reiss timo <t.reiss@eos-ts.com>
2023-06-27 11:48:45 +0200

    added shell for repositories

---
+++ src/main/java/com/assemblyenjoyer1/insanecalculator/repository/IRideRepository.java
@@ -0,0 +1,6 @@
+package com.assemblyenjoyer1.insanecalculator.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRideRepository extends JpaRepository {
+}
---
+++ src/main/java/com/assemblyenjoyer1/insanecalculator/repository/IUserRepository.java
@@ -0,0 +1,7 @@
+package com.assemblyenjoyer1.insanecalculator.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository {
+
+}
