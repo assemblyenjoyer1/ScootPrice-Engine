d24436da9711187de4f1675ecb06a516d2169f0e
Daniel Auer <d.auer@eos-ts.com>
2023-06-28 16:25:10 +0200

    created login

---
+++ frontend-scooteq/src/components/Login.tsx
@@ -0,0 +1,73 @@
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+
+type LoginProps = {
+  onLogin: (loggedIn: boolean) => void;
+};
+
+const Login: React.FC<LoginProps> = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("sending");
+    if (email !== '' && password !== '') {
+      try {
+        const response = await fetch(`http://localhost:8080/api/login/validate`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // body: JSON.stringify({ email, password }), // Add the necessary request body if required
+        });
+        if (response.ok) {
+          const data = await response.json();
+          console.log("User:", data);
+          onLogin(true);
+        }
+      } catch (error) {
+        console.error("Error while calculating: ", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const storedLoggedIn = localStorage.getItem('loggedIn');
+    const parsedLoggedIn = storedLoggedIn ? JSON.parse(storedLoggedIn) : false;
+    onLogin(parsedLoggedIn);
+  }, [onLogin]);
+
+  useEffect(() => {
+    localStorage.setItem('loggedIn', JSON.stringify(false));
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="App-title">
+        <img src={require("./scooteq.png")} alt="png" style={{ height: '300px', width: 'auto', maxWidth: '100%' }} />
+      </div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input className="App-inputText" type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          <input className="App-inputText" type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button className="App-button" type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
