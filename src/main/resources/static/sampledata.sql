4b3d0c24ad3fb439d59c712c3856a32f36737f68
reiss timo <t.reiss@eos-ts.com>
2023-06-30 11:47:26 +0200

    ids are now unique

--- src/main/resources/static/sampledata.sql
+++ src/main/resources/static/sampledata.sql
@@ -17,9 +17,9 @@ CREATE TABLE rides (
   VALUES
       (0, '11111111-1111-1111-1111-111111111111', 'Daniel Auer', 'auerda@yahoo.de', '111'),
       (1, '22222222-2222-2222-2222-222222222222', 'Julien Bedrich', 'julien@yahoo.de', '123'),
-      (1, '22222222-2222-2222-2222-222222222222', 'Holger Schmidt', 'holger@yahoo.de', '111'),
-      (1, '22222222-2222-2222-2222-222222222222', 'Niklas Ehrhardt', 'holger@yahoo.de', '111'),
-      (2, '33333333-3333-3333-3333-333333333333', 'Timo Reiss', 'timo@yahoo.de', '222');
+      (1, '33333333-3333-3333-3333-333333333333', 'Holger Schmidt', 'holger@yahoo.de', '111'),
+      (1, '44444444-4444-4444-4444-444444444444', 'Niklas Ehrhardt', 'holger@yahoo.de', '111'),
+      (2, '55555555-5555-5555-5555-555555555555', 'Timo Reiss', 'timo@yahoo.de', '222');
 
 
  */
