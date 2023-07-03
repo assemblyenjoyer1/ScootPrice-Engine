ec657d291fd2557a26466bf37c3315912dcf16d9
reiss timo <t.reiss@eos-ts.com>
2023-07-03 14:48:58 +0200

    wip

--- src/main/java/com/assemblyenjoyer1/insanecalculator/services/UserService.java
+++ src/main/java/com/assemblyenjoyer1/insanecalculator/services/UserService.java
@@ -42,8 +42,11 @@ public class UserService {
 
     public ResponseEntity<User> registerUser(String email, String name, String password) {
         if(email == null | name == null | password == null) return ResponseEntity.badRequest().body(null);
-        User user = new User(name,email,password);
-        userRepository.save(user);
-        return ResponseEntity.ok().body(user);
+        if(userRepository.findByEmail(email).isEmpty()){
+            User user = new User(name,email,password);
+            userRepository.save(user);
+            return ResponseEntity.ok().body(user);
+        }
+        return ResponseEntity.status(409).body(null);
     }
 }
