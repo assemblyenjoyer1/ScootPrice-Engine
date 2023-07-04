f00cf99aa2950fbdec40217faa84c38cfbb444e3
reiss timo <t.reiss@eos-ts.com>
2023-07-04 11:39:33 +0200

    wip

--- src/main/java/com/assemblyenjoyer1/insanecalculator/config/OpenApiConfig.java
+++ src/main/java/com/assemblyenjoyer1/insanecalculator/config/OpenApiConfig.java
@@ -13,12 +13,12 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact(
-                        name = "Alibou",
-                        email = "contact@aliboucoding.com",
-                        url = "https://aliboucoding.com/course"
+                        name = "ScooTeq",
+                        email = "contact@ScooTeqqus.com",
+                        url = "https://scooteq.com/"
                 ),
                 description = "OpenApi documentation for Spring Security",
-                title = "OpenApi specification - Alibou",
+                title = "OpenApi specification - ScooTeq",
                 version = "1.0",
                 license = @License(
                         name = "Licence name",
@@ -33,7 +33,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 ),
                 @Server(
                         description = "PROD ENV",
-                        url = "https://aliboucoding.com/course"
+                        url = "http://localhost:8080"
                 )
         },
         security = {
