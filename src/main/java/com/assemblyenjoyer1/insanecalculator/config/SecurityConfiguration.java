00b250a832fcda733ae97bbd16b74c1fa91de3af
reiss timo <t.reiss@eos-ts.com>
2023-07-05 12:05:10 +0200

    jwt token works now for managers

--- src/main/java/com/assemblyenjoyer1/insanecalculator/config/SecurityConfiguration.java
+++ src/main/java/com/assemblyenjoyer1/insanecalculator/config/SecurityConfiguration.java
@@ -42,12 +42,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/blog/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
-                        .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
-                        .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
                         .requestMatchers("/api/calculator/**").permitAll()
-                        .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
-                        .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
                         .anyRequest().authenticated()
                 )
 
