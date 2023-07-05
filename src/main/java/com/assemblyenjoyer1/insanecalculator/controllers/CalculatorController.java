c1f1abc8ef5cd8ba66d9287f65a31eb34c3233e3
reiss timo <t.reiss@eos-ts.com>
2023-07-05 12:01:07 +0200

    jwt token works now for managers

--- src/main/java/com/assemblyenjoyer1/insanecalculator/controllers/CalculatorController.java
+++ src/main/java/com/assemblyenjoyer1/insanecalculator/controllers/CalculatorController.java
@@ -1,9 +1,8 @@
 package com.assemblyenjoyer1.insanecalculator.controllers;
 
-import com.assemblyenjoyer1.insanecalculator.user.User;
 import com.assemblyenjoyer1.insanecalculator.repository.UserRepository;
 import com.assemblyenjoyer1.insanecalculator.services.CalculatorService;
-import com.assemblyenjoyer1.insanecalculator.services.UserService;
+import com.assemblyenjoyer1.insanecalculator.user.User;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,21 +10,20 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/calculator")
 @RequiredArgsConstructor
 @CrossOrigin
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasAnyRoleRole('ADMIN', 'MANAGER')")
 public class CalculatorController {
 
     final private CalculatorService calculatorService;
     final private UserRepository userRepository;
 
     @PostMapping("/price/distance")
-    @PreAuthorize("hasAuthority('admin:create')")
+    @PreAuthorize("hasAnyAuthority('admin:create', 'manager:create')")
     @Hidden
     public ResponseEntity<Double> calculatePriceByDistance(@RequestBody CalculatePriceDTO calculatePriceDTO) {
         String userID = calculatePriceDTO.getUserID();
@@ -38,7 +36,7 @@ public class CalculatorController {
     }
 
     @PostMapping("/price/time")
-    @PreAuthorize("hasAuthority('admin:create')")
+    @PreAuthorize("hasAnyAuthority('admin:create', 'management:create')")
     @Hidden
     public ResponseEntity<Double> calculatePriceByTime(@RequestBody CalculatePriceDTO calculatePriceDTO) {
         String userID = calculatePriceDTO.getUserID();
